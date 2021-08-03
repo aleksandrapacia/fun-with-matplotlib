757abcbdcf770a3cec8fc8a5466836727565105d
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-08-03 18:31:16 +0200

    Automatic data couting
    -using functions to calculate the appropriate size of the graph
    -using axis() function

--- scatter.py
+++ scatter.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4,9, 16, 25]
-
+x_values = range(1, 201)
+y_values = [x**2 for x in x_values]
 plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 # tytuł wiersza
 ax.set_title('Kwadraty liczb', fontsize=28)
@@ -14,6 +13,8 @@ ax.set_xlabel('Wartość', fontsize=14)
 ax.set_ylabel('Kwadraty wartości', fontsize=14)
 #wielkość etykiet
 ax.tick_params(axis='both', labelsize=8)
+
+ax.axis([0, 200, 0, 40000])
 
 
 plt.show()
