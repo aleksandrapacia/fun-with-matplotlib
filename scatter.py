5822abc63fd011742ff59bd53b496f40f066f8c1
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-08-04 21:09:04 +0200

    Changed line colour

--- scatter.py
+++ scatter.py
@@ -4,7 +4,7 @@ x_values = range(1, 201)
 y_values = [x**2 for x in x_values]
 plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values,c=(0, 0.5, 0), s=10)
 
 # tytuł wiersza
 ax.set_title('Kwadraty liczb', fontsize=28)
