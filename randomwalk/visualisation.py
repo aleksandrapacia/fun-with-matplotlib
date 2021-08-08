338d2cc875ae5b4294100d2b9ad86c1bb5cadd62
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-08-08 13:17:41 +0200

    scatters improvements*, axis are not visible
    * - green scatter is the first that was created and the red scatter is the last  one

--- randomwalk/visualisation.py
+++ randomwalk/visualisation.py
@@ -9,8 +9,11 @@ while True:
     fig, ax = plt.subplots()
     point_numbers = range(rndm.numberOfPoints)
     ax.scatter(rndm.xs, rndm.ys, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none',s=15)
-    ax.scatter(0, 0, c='green', edgecolors='none', s=10)
-    ax.scatter(rndm.xs[-1], rndm.ys[-1], c='red', edgecolors='none', s=10)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=60)
+    ax.scatter(rndm.xs[-1], rndm.ys[-1], c='red', edgecolors='none', s=60)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
     
 
