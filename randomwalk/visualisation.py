0e9f3e277239c26d1a987ba31e987c1867eddeb7
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-08-08 13:11:11 +0200

    dispalying first created scatter and the last one (emphasised with colors)

--- randomwalk/visualisation.py
+++ randomwalk/visualisation.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt 
 from randomm import RandomWalk
 
-rndm = RandomWalk()
-rndm.walkk()
+while True:
+    rndm = RandomWalk()
+    rndm.walkk()
 
-plt.style.use('seaborn-pastel')
-fig, ax = plt.subplots()
-ax.scatter(rndm.xs, rndm.ys, s=15)
-plt.show()
+    plt.style.use('seaborn-pastel')
+    fig, ax = plt.subplots()
+    point_numbers = range(rndm.numberOfPoints)
+    ax.scatter(rndm.xs, rndm.ys, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none',s=15)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=10)
+    ax.scatter(rndm.xs[-1], rndm.ys[-1], c='red', edgecolors='none', s=10)
+    plt.show()
+    
+
+    keep_running = input("Do you want to create new random walk? If yes write 'Y', if no write 'N': ")
+    if keep_running == 'N':
+        break
