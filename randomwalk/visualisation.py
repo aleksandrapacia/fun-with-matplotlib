16db0c6813b7b132f36e6772237128b2f0b64dda
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-08-08 13:26:49 +0200

    graph fits the screen, added more scatters

--- randomwalk/visualisation.py
+++ randomwalk/visualisation.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 from randomm import RandomWalk
 
 while True:
-    rndm = RandomWalk()
+    rndm = RandomWalk(70_000)
     rndm.walkk()
 
     plt.style.use('seaborn-pastel')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15,9))
     point_numbers = range(rndm.numberOfPoints)
-    ax.scatter(rndm.xs, rndm.ys, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none',s=15)
-    ax.scatter(0, 0, c='green', edgecolors='none', s=60)
-    ax.scatter(rndm.xs[-1], rndm.ys[-1], c='red', edgecolors='none', s=60)
+    ax.scatter(rndm.xs, rndm.ys, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none',s=10)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=15)
+    ax.scatter(rndm.xs[-1], rndm.ys[-1], c='red', edgecolors='none', s=15)
 
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
