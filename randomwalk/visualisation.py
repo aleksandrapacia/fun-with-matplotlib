2c3471a1ad997e32acf0764aa6d0bcb39f300cef
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-08-08 13:35:44 +0200

    flower pollen path on the water

--- randomwalk/visualisation.py
+++ randomwalk/visualisation.py
@@ -2,15 +2,16 @@ import matplotlib.pyplot as plt
 from randomm import RandomWalk
 
 while True:
-    rndm = RandomWalk(70_000)
+    rndm = RandomWalk(5000)
     rndm.walkk()
 
     plt.style.use('seaborn-pastel')
     fig, ax = plt.subplots(figsize=(15,9))
+    ax.set_title('flower pollen path on the water')
     point_numbers = range(rndm.numberOfPoints)
-    ax.scatter(rndm.xs, rndm.ys, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none',s=10)
-    ax.scatter(0, 0, c='green', edgecolors='none', s=15)
-    ax.scatter(rndm.xs[-1], rndm.ys[-1], c='red', edgecolors='none', s=15)
+    ax.plot(rndm.xs, rndm.ys, linewidth=1)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rndm.xs[-1], rndm.ys[-1], c='red', edgecolors='none', s=100)
 
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
